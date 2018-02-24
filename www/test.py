3fb24246968e69256293a68a9eb58b2c6fe7bc51
savoy <wanglu396@126.com>
2018-02-24 15:26:31 +0800

    add init

---
+++ www/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'Savoy Wang'
+
+
+import orm, asyncio
+from models import User, Blog, Comment
+
+
+loop = asyncio.get_event_loop()
+
+
+async def test():
+    await orm.create_pool(loop=loop, user='root', password='root', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+
+loop.run_until_complete(test())
